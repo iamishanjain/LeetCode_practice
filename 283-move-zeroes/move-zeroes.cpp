351387b3e67f1dade9812c6e8077f33792d2365a
iamishanjain <112146348+iamishanjain@users.noreply.github.com>
2025-03-17 10:06:24 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (78.40%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,24 +1,46 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-
+       // 1 -> Brute Force
+       
         // int size = nums.size();
-        // int j = -1;
+        // vector<int> temp;
         // for(int i = 0; i<size; i++){
-        //     if(nums[i] == 0){
-        //         j = i;
-        //         break;
+        //     if(nums[i] != 0){
+        //         temp.push_back(nums[i]);
         //     }
         // }
-        // for(int i = j+1; i< size; i++){
+        // int nz = temp.size();
+        // for(int i = 0; i<nz; i++){
+        //     nums[i] = temp[i];
+        // }
+        // for(int i = nz; i< size; i++){
+        //     nums[i] = 0;
+        // }
+        // // return nums;
+
+    // 2 -> Better Approach
+
+        // int j = 0;
+        // for(int i = 0; i< nums.size(); i++){
         //     if(nums[i] != 0){
         //         swap(nums[i], nums[j]);
         //         j++;
         //     }
         // }
 
-        int j = 0;
-        for(int i = 0; i< nums.size(); i++){
+    // 3-> Optimal
+
+        int size = nums.size();
+        int j = -1;
+        for(int i = 0; i<size; i++){
+            if(nums[i] == 0){
+                j = i;
+                break;
+            }
+        }
+        if(j == -1) return;
+        for(int i = j+1; i< size; i++){
             if(nums[i] != 0){
                 swap(nums[i], nums[j]);
                 j++;
